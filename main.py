c9af023b39d42787d653511e3b7c400d5c9964c1
nahuel893 <nahuel893@hotmail.com>
2025-10-23 19:15:06 -0300

    deleted coments

--- main.py
+++ main.py
@@ -14,12 +14,14 @@ print(ABS_PATH)
 #print(FATHER_PATH)
 print(DATA_PATH)
 
+
 """
     TO-DO:
     * Import and export to csv
     * Add tests
     * Create Sales class
 """
+
 def main(): # Creamos una instancia de la clase Endpoint
     load_dotenv()
     
@@ -56,41 +58,6 @@ def main(): # Creamos una instancia de la clase Endpoint
     branches = [1, 3, 4, 5, 6, 7, 9, 10, 11, 12, 13, 14, 15, 16]
     endp.obtener_reporte(
         branches, "ventas_hasta_hoy", "2025-10-01", "2025-10-31")
-    # Get report of sales
-
-    # Realizamos el login
-    # response = endpoint.login()
-    # Obtenemos el reporte
-    # start_date = datetime(2025, 4, 1)
-    # end_date = datetime(2025, 4, 30)  # Adjust this date as needed
-
-    # current_date = start_date
-
-    # # Hacer peticiones por mes
-    # while current_date < end_date:
-    #     next_month = current_date + timedelta(days=31)
-    #     next_month = next_month.replace(day=1)
-
-    #     fecha_inicio = current_date.strftime("%Y-%m-%d")
-    #     fecha_fin = (next_month - timedelta(days=1)).strftime("%Y-%m-%d")
-        
-    #     print('fecha inicio:', fecha_inicio)
-    #     print('fecha fin:', fecha_fin)
-
-    #     nombre_mes = current_date.strftime("%B")
-    #     nombre_anio = current_date.strftime("%Y")
-
-    #     print('nombre del mes:', nombre_mes)
-    #     print('nombre del anio:', nombre_anio)
-
-    #     name = f'Reporte {nombre_mes} {nombre_anio}'
-    #     endpoint.obtener_reporte(name, fecha_inicio, fecha_fin)
-    #     current_date = next_month
-
-      
-    #     # Luego cada mes procesarlo, dejar columnas importanes y guardarlo.
-    #     ruta_archivo = os.path.join( FATHER_PATH+ "\\" + name + '.xls')
-    #     data.delete_bloat_columns(ruta_archivo) 
 
 
 if __name__ == "__main__":
